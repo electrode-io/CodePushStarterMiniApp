fe32c15df83bb7be9e1837b679cf55cc89a97dac
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2018-05-16 16:41:03 -0700

    Initial commit

---
+++ android/app/src/main/java/com/walmartlabs/ern/MainApplication.java
@@ -0,0 +1,21 @@
+package com.walmartlabs.ern;
+
+import android.app.Application;
+
+import com.walmartlabs.ern.container.ElectrodeReactContainer;
+import com.walmartlabs.ern.container.plugins.CodePushPlugin;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ElectrodeReactContainer.initialize(
+                this,
+                new ElectrodeReactContainer.Config().isReactNativeDeveloperSupport(RunnerConfig.RN_DEV_SUPPORT_ENABLED),
+                new CodePushPlugin.Config("NONE"));
+
+    }
+
+}
